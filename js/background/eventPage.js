fda12b50b7cc23e7ed84f06fa654e203e1e89312
luwenxull <louislu@taiyear.com>
2016-11-08 18:39:27 +0800

    command test

--- js/background/eventPage.js
+++ js/background/eventPage.js
@@ -6,11 +6,15 @@ var strategies = {
         chrome.tabs.create({
             url:value
         })
+    },
+    "append_input":function(){
+        console.log('ai');
     }
 }
 
 chrome.runtime.onMessage.addListener(function (request,sender,response) {
-    var type = request.type,
-        content = request.content;
-        response(strategies[type].call(null,content))
+    console.log(request);
+    // var type = request.type,
+    //     content = request.content;
+    //     response(strategies[type].call(null,content))
 });
