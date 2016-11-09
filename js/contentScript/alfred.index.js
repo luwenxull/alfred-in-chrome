bbec8b934a49a2d30d90208194005b9168ea13cc
luwenxull <992273307@qq.com>
2016-11-09 21:12:53 +0800

    for mac

--- js/contentScript/alfred.index.js
+++ js/contentScript/alfred.index.js
@@ -2,8 +2,9 @@ console.log('welcome to alfred-in-chrome');
 
 document.body.addEventListener('keydown', function (e) {
 	var key = e.key,
-		alt = e.altKey;
-	if (key == ',' && alt) {
+		code = e.code
+	alt = e.altKey;
+	if ((key == ',' || code == 'Comma') && alt) {
 		e.stopPropagation();
 		e.preventDefault();
 		createAlfred()
